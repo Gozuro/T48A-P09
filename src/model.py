4f667d180eaa7fa82cc7417588e011b769595773
Gozuro <160263006+Gozuro@users.noreply.github.com>
2025-10-10 19:34:59 -0600

    Update model.py

--- src/model.py
+++ src/model.py
@@ -4,9 +4,15 @@ from sklearn.naive_bayes import MultinomialNB
 
 def create_model():
     pass
+    model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+    return model
 
 def train_model(model, data, target):
     pass
+    model.fit(data, target)
+    return model
 
 def predict(model, data):
     pass
+    model.fit(data, target)
+    return model
